55d7cca6e06b05abef26ceb7d2ffaeb1c38ed00c
Millie <millienakiganda@gmail.com>
2022-12-02 20:52:05 +0300

    chore: useTextEditingController

--- flutter_test_hooks/lib/main.dart
+++ flutter_test_hooks/lib/main.dart
@@ -21,19 +21,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Stream<String> getTime() => Stream.periodic(
-    const Duration(seconds: 1), (_) => DateTime.now().toIso8601String());
-
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
+    final controller = useTextEditingController();
+    final text = useState('');
+    useEffect(() {
+      controller.addListener(() {
+        text.value = controller.text;
+      });
+      return null;
+    }, [controller]);
     return Scaffold(
       appBar: AppBar(
-        title: Text(dateTime.data ?? 'Home Page'),
+        title: const Text('Home Page'),
       ),
+      body: Column(children: [
+        TextField(
+          controller: controller,
+        ),
+        Text('You typed ${text.value}')
+      ]),
     );
   }
 }
