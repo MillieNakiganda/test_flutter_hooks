d4d34b45e253c7b8d4e8682b4fbda8e6a7044087
Millie <millienakiganda@gmail.com>
2022-12-11 17:48:38 +0300

    useReducer hook

--- flutter_test_hooks/lib/main.dart
+++ flutter_test_hooks/lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+//Reducer is a combination of state and actions, NewState = oldState + Action
 
 extension CompactMap<T> on Iterable<T?> {
   Iterable<T> compactMap<E>([
@@ -12,6 +12,40 @@ extension CompactMap<T> on Iterable<T?> {
       map(
         transform ?? (e) => e,
       ).where((e) => e != null).cast();
+}
+
+enum Action { rotateLeft, rotateRight, moreVisible, lessVisible }
+
+@immutable
+class State {
+  final double rotationDeg;
+  final double alpha;
+  const State({required this.rotationDeg, required this.alpha});
+  const State.zero()
+      : rotationDeg = 0.0,
+        alpha = 1.0;
+
+  State rotateRight() => State(alpha: alpha, rotationDeg: rotationDeg + 10.0);
+  State rotateLeft() => State(alpha: alpha, rotationDeg: rotationDeg - 10.0);
+  State increaseAlpha() =>
+      State(alpha: min(alpha + 0.1, 1.0), rotationDeg: rotationDeg + 10.0);
+  State decreaseAlpha() =>
+      State(alpha: max(alpha - 0.1, 1.0), rotationDeg: rotationDeg + 10.0);
+}
+
+State reducer(State oldState, Action? action) {
+  switch (action) {
+    case Action.rotateLeft:
+      return oldState.rotateLeft();
+    case Action.rotateRight:
+      return oldState.rotateRight();
+    case Action.moreVisible:
+      return oldState.increaseAlpha();
+    case Action.lessVisible:
+      return oldState.decreaseAlpha();
+    case null:
+      return oldState;
+  }
 }
 
 void main() {
@@ -41,32 +75,47 @@ class MyHomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-    controller = useStreamController<double>(
-      onListen: () {
-        controller.sink.add(0);
-      },
-    );
+    final store = useReducer<State, Action?>(reducer,
+        initialState: const State.zero(), initialAction: null);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
-        body: StreamBuilder<double>(
-            stream: controller.stream,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const CircularProgressIndicator();
-              } else {
-                final rotation = snapshot.data ?? 0;
-                return GestureDetector(
-                  onTap: () {
-                    controller.sink.add(rotation + 10.0);
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    store.dispatch(Action.rotateLeft);
                   },
-                  child: RotationTransition(
-                      turns: AlwaysStoppedAnimation(rotation / 360.0),
-                      child: Center(child: Image.network(url))),
-                );
-              }
-            }));
+                  child: const Text('Rotate Left')),
+              TextButton(
+                  onPressed: () {
+                    store.dispatch(Action.rotateRight);
+                  },
+                  child: const Text('Rotate Right')),
+              TextButton(
+                  onPressed: () {
+                    store.dispatch(Action.lessVisible);
+                  },
+                  child: const Text('-Alpha')),
+              TextButton(
+                  onPressed: () {
+                    store.dispatch(Action.moreVisible);
+                  },
+                  child: const Text('+ Alpha'))
+            ],
+          ),
+          const SizedBox(height: 100),
+          Opacity(
+            opacity: store.state.alpha,
+            child: RotationTransition(
+                turns: AlwaysStoppedAnimation(store.state.rotationDeg / 360.0),
+                child: Image.network(url)),
+          ),
+        ],
+      ),
+    );
   }
 }
